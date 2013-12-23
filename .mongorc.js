845ca3969aee3f165f18fedc2b9135555515fb79
Anemos <kissanemos@gmail.com>
2013-12-23 18:48:07 +0900

    test

---
+++ .mongorc.js
@@ -0,0 +1,7 @@
+function insertData(dbName, colName, num) {
+  var col = db.getSiblingDB(dbName).getCollection(colName);
+  for (i = 0; i < num; i++) {
+    col.insert({x:i});
+  }
+  print(col.count());
+}
