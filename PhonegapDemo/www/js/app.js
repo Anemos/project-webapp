5b3e165b45394889d5d93980655ac189013b9004
Anemos <kissanemos@gmail.com>
2013-12-24 18:16:39 +0900

    back button 사용시에 그림파일이 안보이는 현상 계속발생

--- PhonegapDemo/www/js/app.js
+++ PhonegapDemo/www/js/app.js
@@ -21,18 +21,3 @@ phonecatApp.config(['$routeProvider',
         redirectTo: '/phones'
       });
   }]);
-  
-phonecatApp.run(function($rootScope, $route, $location){
-   //Bind the `$locationChangeSuccess` event on the rootScope, so that we dont need to 
-   //bind in induvidual controllers.
-
-   $rootScope.$on('$locationChangeSuccess', function() {
-        $rootScope.actualLocation = $location.path();
-    });        
-
-   $rootScope.$watch(function () {return $location.path()}, function (newLocation, oldLocation) {
-        if($rootScope.actualLocation === newLocation) {
-            alert('Why did you use history back?');
-        }
-    });
-});
